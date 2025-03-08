7cf54ef614afe2e1e05d723265ddc2bfb3b6662e
bouchta65 <bouchtamohamed01@gmail.com>
2025-03-08 22:43:37 +0000

    Add Assignment model to manage ticket assignments

---
+++ app/Models/Assignment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Assignment extends Model
+{
+    use HasFactory;
+
+    protected $table = 'assignment';
+
+    protected $fillable = [
+        'user_id',
+        'ticket_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+}
